32af6ef4ec8f6be2a81b558ef8342e1a661871ce
Bo Shui <boshui@gmail.com>
2024-10-09 19:07:27 +0200

    added a new class Player, but do not know why giving it name Player. added some strings and added a countUp method.

---
+++ Test/Player.java
@@ -0,0 +1,21 @@
+public class Player {
+
+    String name;
+
+    String type;
+
+    int max = 10;
+
+    int value = 0;
+
+    String message = "This is the last message, can not go over 12.";
+
+    public int countUp2(){
+        value +=2;
+        System.out.println("value");
+        return value;
+    }
+
+
+
+}
--- Test/Test.java
+++ Test/Test.java
@@ -4,5 +4,7 @@ public class Test {
         System.out.println("Hello World!");
 
         System.out.println("Hallo Git. ");
+
+        System.out.println("now it is the Git World. 19:00 exact time.");
     }
 }
